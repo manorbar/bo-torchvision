b8c7157d6879a0dbf1b56095315a62c1a92b599c
Manor Bar <manorbar12@gmail.com>
2022-09-25 10:18:07 +0300

    fixed tests deprecation

--- test/test_prototype_datasets_builtin.py
+++ test/test_prototype_datasets_builtin.py
@@ -101,12 +101,11 @@ class TestCommon:
 
         next(iter(dataset.map(transforms.Identity())))
 
-    @pytest.mark.parametrize("only_datapipe", [False, True])
     @parametrize_dataset_mocks(DATASET_MOCKS)
-    def test_traversable(self, dataset_mock, config, only_datapipe):
+    def test_traversable(self, dataset_mock, config):
         dataset, _ = dataset_mock.load(config)
 
-        traverse(dataset, only_datapipe=only_datapipe)
+        traverse(dataset)
 
     @parametrize_dataset_mocks(DATASET_MOCKS)
     def test_serializable(self, dataset_mock, config):
